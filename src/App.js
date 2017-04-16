b804d814e293ae8f22ffc54807013054c1088a37
Kia Farhang <kfarhang0@gmail.com>
2017-04-15 21:07:19 -0700

    Created container component to fetch people from an API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React, { Component } from 'react';
-import PersonList from './PersonList'
+import PersonListContainer from './PersonListContainer';
 import './App.css';
 
 class App extends Component {
   render() {
-    const kia = {
-      name: 'Kia',
-      src: 'https://kiafarhang.com/img/kf.jpg',
-      email: 'kia@google.com',
-      phone: '(123) 456-7890'
-    };
-
-    const ashley = {
-      name: 'Ashley',
-      src: 'https://randomuser.me/api/portraits/women/87.jpg',
-      email: 'ashley@facebook.com',
-      phone: '(555) 555-4567'
-    }
-    const thePeople = [kia, ashley];
     return (
-      <PersonList people={thePeople}/>
+      <PersonListContainer />
     );
   }
 }
---
+++ src/PersonListContainer.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import PersonList from './PersonList';
+
+class PersonListContainer extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    var th = this;
+    const url = 'https://randomuser.me/api/?results=10';
+
+    fetch(url)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(json){
+      let dataArray = [];
+      json.results.forEach(function(person){
+        let personObject = {
+          name: `${person.name.first} ${person.name.last}`,
+          src: `${person.picture.large}`,
+          email: `${person.email}`,
+          phone: `${person.cell}`
+        };
+        dataArray.push(personObject);
+      });
+      th.setState({data: dataArray});
+    })
+    .catch(function(){
+      })
+  }
+
+
+  render() {
+    return (
+      <div className="PersonListContainer">
+        <PersonList people={this.state.data}/>
+      </div>
+    );
+  }
+}
+
+export default PersonListContainer;
