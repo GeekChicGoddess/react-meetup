ca102cc6a607c4012b8448e86bbcbab046780707
Kia Farhang <kfarhang0@gmail.com>
2017-07-09 10:43:32 -0500

    Some styling work

--- src/Person.js
+++ src/Person.js
@@ -5,10 +5,14 @@ class Person extends Component {
   render() {
     return (
       <div className="Person">
-        <img className="Person-image" src={this.props.src} alt={this.props.name}/>
-        <h3 className="Person-name">{this.props.name}</h3>
-        <p>{this.props.email}</p>
-        <p>{this.props.phone}</p>
+        <div>
+          <img className="Person-image" src={this.props.src} alt={this.props.name}/>
+        </div>
+        <div className='Person-info'>
+          <h3 className="Person-name">{this.props.name}</h3>
+          <p>{this.props.email}</p>
+          <p>{this.props.phone}</p>
+        </div>
       </div>
     );
   }
